593578f4b595f0e483f9b7204af604d31bc82a20
BruneMejia <madeline.mej07@gmail.com>
2026-03-17 16:02:46 -0600

    page

--- Laboratorio1Comp2/vite.config.js
+++ Laboratorio1Comp2/vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Laboratorio1Comp2/',
   plugins: [
     vue(),
     vueDevTools(),
